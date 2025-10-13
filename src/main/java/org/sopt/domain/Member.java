0032804e672ebb2b1a7e77342c99344a6480a255
Choi Min Young <codingmy@naver.com>
2025-10-14 04:10:29 +0900

    feat/#2: 도메인에 회원 정보 항목 추가

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -1,13 +1,21 @@
 package org.sopt.domain;
 
+import java.sql.Timestamp;
+
 public class Member {
 
     private Long id;
     private String name;
+    private String email;
+    private Timestamp birthDate;
+    private Gender gender;
 
-    public Member(Long id, String name) {
+    public Member(Long id, String name, String email, Timestamp birthDate, Gender gender) {
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.gender = gender;
     }
 
     public Long getId() {
@@ -16,5 +24,17 @@ public class Member {
 
     public String getName() {
         return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Timestamp getBirthDate() {
+        return birthDate;
+    }
+
+    public Gender getGender() {
+        return gender;
     }
 }
