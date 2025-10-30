40eda20697de998f172c8b8136e8f325d4b03868
Choi Min Young <codingmy@naver.com>
2025-10-30 14:36:02 +0900

    feat/#4: 에러코드 추가

--- src/main/java/org/sopt/common/exception/ErrorCode.java
+++ src/main/java/org/sopt/common/exception/ErrorCode.java
@@ -9,8 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 정보를 찾을 수 없습니다."),
+
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 가입된 이메일입니다."),
+
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "요청 값이 올바르지 않습니다."),
+    UNDERAGE_MEMBER(HttpStatus.BAD_REQUEST, "20세 미만은 가입할 수 없습니다."),
+
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus status;
